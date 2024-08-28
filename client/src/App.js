f327daedb6abc00231627b0fa110af89ad491258
balaji <somarouthubalaji@gmail.com>
2024-08-28 15:18:55 +0530

    updated code

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+
 import About from './components/About';
 import Education from './components/Education';
 import Skills from './components/Skills';
+
 import Contact from './components/Contact';
 
 import './App.css';
+
 
 function App() {
   return (
